b2ea05a649b37d9b0f21ab6c4ef8cca65712e279
leehyobeom <dlgyqja104@gmail.com>
2026-02-26 13:40:25 +0900

    fix: db

--- src/plan/plan.controller.ts
+++ src/plan/plan.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Put, Query } from '@nestjs/common';
 import { PlanService } from './plan.service';
 
 @Controller('plan')
 export class PlanController {
     constructor(private readonly planService: PlanService) { }
 
-    @Get()
+    @Put()
     async getPlans(@Query('limit') limit?: string) {
         const parsedLimit = limit ? parseInt(limit, 10) : 1000;
         return this.planService.getTopPlans(parsedLimit);
